b1736366f66beff4d119e02f4d92892c61b4a26a
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2024-02-02 16:18:00 -0500

    fetching

---
+++ index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios' 
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'; 
+
+axios.get(url).then( response => {
+  const todo = response.data; 
+
+  const id = todo.id; 
+  const title = todo.title; 
+  const finished = todo.completed 
+
+  console.log(`
+    The todo with id: ${id}
+    Has a title of: ${title}
+    Is it finished: ${finished}
+  `)
+})
