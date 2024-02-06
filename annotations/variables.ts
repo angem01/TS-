d687193b113774628d1e4d97ce33e096143162fd
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2024-02-06 12:11:58 -0500

    object literal annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -7,3 +7,23 @@ let nothing: undefined= undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Arrays 
+let myNumbers: number[] = [1, 2, 3, 4]; 
+let colors: string[] = ['blue', 'red', 'green']
+let truths: boolean[] = [false, true, false]
+
+// classes 
+
+class Car {
+
+}
+
+let car: Car = new Car()
+
+// Object literal 
+
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 20
+}
