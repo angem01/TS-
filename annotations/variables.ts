6c4351533259305d1da826f7646c3c99d71eaf0e
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2024-02-06 15:35:25 -0500

    fixing the any type

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -37,6 +37,6 @@ const logNumber: (i: number) => void = (i) => {
 // 1) Function that returns the Any type
 
 const json = '{"x": 10, "y": 20}'
-const coordinates = JSON.parse(json)
+const coordinates: {x: number, y: number} = JSON.parse(json)
 
 console.log(coordinates)
